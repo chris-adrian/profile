d04fadfb68ba3b0e8807e9f48fe158425926d4ac
Kurisu <james.utilityuse@gmail.com>
2021-09-25 22:27:17 +0900

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+export interface Props {
+  heading: string;
+  subHeading?: string;
+}
+
+const Header = (props: Props) => {
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      {props.subHeading ? `<h2>${props.subHeading}</h2>` : ""}
+    </div>
+  );
+};
+
+export default Header;
